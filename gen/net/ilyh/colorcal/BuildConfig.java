83815deca820c6ce30bcbe17a6db621a216eaa94
ilyh <chenbo515@gmail.com>
2014-03-07 22:48:43 +0800

    Initial commit

---
+++ gen/net/ilyh/colorcal/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.ilyh.colorcal;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/net/ilyh/colorcal/MainActivity.java
@@ -0,0 +1,52 @@
+package net.ilyh.colorcal;
+
+import android.os.Bundle;
+import android.R.integer;
+import android.R.string;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends Activity
+{
+
+	View[] view=new View[5];
+	Spinner[] spinner=new Spinner[5];
+	String[] ringColorsStrings={"RED","BLUE","GERY","YELLOW"};
+	ArrayAdapter<String> arrayAdapter;
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		arrayAdapter=new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, ringColorsStrings);
+		view[0]=findViewById(R.id.view1);
+		view[1]=findViewById(R.id.view2);
+		view[2]=findViewById(R.id.view3);
+		view[3]=findViewById(R.id.view4);
+		view[4]=findViewById(R.id.view5);
+		
+		view[0].setBackgroundColor(Color.RED);
+		view[1].setBackgroundColor(Color.BLACK);
+		view[2].setBackgroundColor(Color.BLACK);
+		view[3].setBackgroundColor(Color.BLUE);
+		view[4].setBackgroundColor(Color.YELLOW);
+		
+		spinner[0]=(Spinner)findViewById(R.id.spinner1);
+		arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner[0].setAdapter(arrayAdapter);
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
