e9ad433e19ca1ee749daec5fa164e83cdc3ff06b
ilyh <chenbo515@gmail.com>
2014-03-11 21:01:57 +0800

    Add spinner listener and radiobutton

--- gen/net/ilyh/colorcal/R.java
+++ gen/net/ilyh/colorcal/R.java
@@ -24,13 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f08000e;
+        public static final int radiobutton1=0x7f08000b;
+        public static final int radiobutton2=0x7f08000c;
+        public static final int radiogroup1=0x7f08000a;
         public static final int spinner1=0x7f080005;
         public static final int spinner2=0x7f080006;
         public static final int spinner3=0x7f080007;
         public static final int spinner4=0x7f080008;
         public static final int spinner5=0x7f080009;
-        public static final int textView1=0x7f08000a;
+        public static final int textView1=0x7f08000d;
         public static final int view1=0x7f080000;
         public static final int view2=0x7f080001;
         public static final int view3=0x7f080002;
--- src/net/ilyh/colorcal/MainActivity.java
+++ src/net/ilyh/colorcal/MainActivity.java
@@ -1,23 +1,65 @@
 package net.ilyh.colorcal;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.os.Bundle;
+import android.R.bool;
+import android.R.color;
 import android.R.integer;
 import android.R.string;
 import android.app.Activity;
 import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity
 {
 
+	private String calRes(int ringOne,int ringTwo,int ringThree,int ringFour)
+	{
+		return "null";
+	}
+	
+	private void refreshUI()
+	{
+		for(int i=0;i<view.length;i++)
+			view[i].setBackgroundColor(Color.parseColor(ColorHexValue.get(spinner[i].getSelectedItem().toString())));
+		
+	}
+	
 	View[] view=new View[5];
 	Spinner[] spinner=new Spinner[5];
-	String[] valueString={"Black","Brown","Red","Orange","Yellow","Green","Blue","Purple","Grey","White"};
-	String[] multiplyStrings={"Black","Brown","Red","Orange","Yellow","Green","Blue","Purple","Grey","White","Golden","Silver"};
-	String[] accurateStrings={"Brown","Red","Golden","None"};
+	RadioButton radioButton1,radioButton2;
+	RadioGroup radioGroup;
+	String[] valueString={"Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Grey","White"};
+	String[] multiplyStrings={"Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Grey","White","Gold","Silver"};
+	String[] accurateStrings={"Brown","Red","Gold"};
+	@SuppressWarnings("serial")
+	Map<String, String> ColorHexValue=new HashMap<String,String>()
+	{{
+		put("Black", "#000000");
+		put("Brown", "#964B00");
+		put("Red", "#FF0000");
+		put("Orange", "#FFA500");
+		put("Yellow","#FFFF00");
+		put("Green", "#0x80000");
+		put("Blue", "#0000FF");
+		put("Violet","#800080");
+		put("Grey","#808080");
+		put("White", "#FF0000");
+		put("Gold", "#FFD700");
+		put("Silver","#C0C0C0");
+	}};
+	boolean is4RingMode;
 	ArrayAdapter<String> valueStringAdapter,multiplyStringAdapter,accurateStringAdapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -57,6 +99,59 @@ public class MainActivity extends Activity
 		spinner[2].setAdapter(valueStringAdapter);
 		spinner[3].setAdapter(multiplyStringAdapter);
 		spinner[4].setAdapter(accurateStringAdapter);
+		
+		radioButton1=(RadioButton) findViewById(R.id.radiobutton1);
+		radioButton2=(RadioButton) findViewById(R.id.radiobutton2);
+		//radioButton1.setSelected(true);
+		view[2].setVisibility(View.GONE);
+		spinner[2].setVisibility(View.GONE);
+		is4RingMode=true;
+		refreshUI();
+		radioGroup=(RadioGroup)findViewById(R.id.radiogroup1);
+		radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+		{
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId)
+			{
+				// TODO Auto-generated method stub
+				if(checkedId==radioButton1.getId())
+				{
+					view[2].setVisibility(View.GONE);
+					spinner[2].setVisibility(View.GONE);
+					is4RingMode=true;
+				}
+				if(checkedId==radioButton2.getId())
+				{
+					view[2].setVisibility(View.VISIBLE);
+					spinner[2].setVisibility(View.VISIBLE);
+					is4RingMode=false;
+				}
+			}
+		});
+		
+		for(int i=0;i<view.length;i++)
+		{
+			spinner[i].setOnItemSelectedListener(new OnItemSelectedListener()
+			{
+
+				@Override
+				public void onItemSelected(AdapterView<?> arg0, View arg1,
+						int arg2, long arg3)
+				{
+					// TODO Auto-generated method stub
+					refreshUI();
+				}
+
+				@Override
+				public void onNothingSelected(AdapterView<?> arg0)
+				{
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		}
+
 	
 	}
 
