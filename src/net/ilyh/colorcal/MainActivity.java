009d9c0a81e5315bdab3d4f654ec2bfcd405d440
ilyh <chenbo515@gmail.com>
2014-03-12 21:30:23 +0800

    Implement basic features.

--- src/net/ilyh/colorcal/MainActivity.java
+++ src/net/ilyh/colorcal/MainActivity.java
@@ -20,7 +20,8 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.RadioGroup;
-
+import android.widget.TextView;
+import java.lang.Math;
 public class MainActivity extends Activity
 {
 
@@ -31,18 +32,53 @@ public class MainActivity extends Activity
 	
 	private void refreshUI()
 	{
+		double res_value;
 		for(int i=0;i<view.length;i++)
 			view[i].setBackgroundColor(Color.parseColor(ColorHexValue.get(spinner[i].getSelectedItem().toString())));
 		
+		if(is4RingMode)
+		{
+			res_value=10*Arrays.asList(valueString).indexOf(spinner[0].getSelectedItem())
+			+Arrays.asList(valueString).indexOf(spinner[1].getSelectedItem());
+			
+		}
+		else
+		{
+			res_value=100*Arrays.asList(valueString).indexOf(spinner[0].getSelectedItem())
+			+10*Arrays.asList(valueString).indexOf(spinner[1].getSelectedItem())
+			+Arrays.asList(valueString).indexOf(spinner[2].getSelectedItem());
+		}
+		int mul_value;
+		mul_value=Arrays.asList(multiplyStrings).indexOf(spinner[3].getSelectedItem());
+		if(mul_value>9)
+		{
+			res_value=res_value/(Math.pow(10, mul_value-9));
+		}
+		else
+		{
+			res_value=res_value*Math.pow(10, mul_value);
+		}
+		String res_string,accuracy_string;
+		if(res_value<1000) res_string=String.format("%f次", res_value);
+		else if(res_value<10E6) res_string=String.format("%fk次",res_value/1000);
+		else if(res_value<10E9) res_string=String.format("%fM次",res_value/10E6);
+		else res_string=String.format("%fG次", res_value/10E9);
+		
+		if(spinner[4].getSelectedItem().toString()=="Brown") accuracy_string="㊣1%";
+		else if(spinner[4].getSelectedItem().toString()=="Red") accuracy_string="㊣2%";
+		else if(spinner[4].getSelectedItem().toString()=="Gold") accuracy_string="㊣5%";
+		else accuracy_string="㊣20%";
+		textView.setText("Resistance value="+res_string+"\r\nAccuracy="+accuracy_string);
 	}
 	
 	View[] view=new View[5];
 	Spinner[] spinner=new Spinner[5];
 	RadioButton radioButton1,radioButton2;
 	RadioGroup radioGroup;
+	TextView textView;
 	String[] valueString={"Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Grey","White"};
 	String[] multiplyStrings={"Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Grey","White","Gold","Silver"};
-	String[] accurateStrings={"Brown","Red","Gold"};
+	String[] accurateStrings={"Brown","Red","Gold","None"};
 	@SuppressWarnings("serial")
 	Map<String, String> ColorHexValue=new HashMap<String,String>()
 	{{
@@ -51,13 +87,14 @@ public class MainActivity extends Activity
 		put("Red", "#FF0000");
 		put("Orange", "#FFA500");
 		put("Yellow","#FFFF00");
-		put("Green", "#0x80000");
+		put("Green", "#008000");
 		put("Blue", "#0000FF");
 		put("Violet","#800080");
 		put("Grey","#808080");
-		put("White", "#FF0000");
+		put("White", "#FFFFFF");
 		put("Gold", "#FFD700");
 		put("Silver","#C0C0C0");
+		put("None", "#808080");
 	}};
 	boolean is4RingMode;
 	ArrayAdapter<String> valueStringAdapter,multiplyStringAdapter,accurateStringAdapter;
@@ -81,11 +118,7 @@ public class MainActivity extends Activity
 		view[3]=findViewById(R.id.view4);
 		view[4]=findViewById(R.id.view5);
 		
-		view[0].setBackgroundColor(Color.RED);
-		view[1].setBackgroundColor(Color.BLACK);
-		view[2].setBackgroundColor(Color.BLACK);
-		view[3].setBackgroundColor(Color.BLUE);
-		view[4].setBackgroundColor(Color.YELLOW);
+
 		
 		spinner[0]=(Spinner)findViewById(R.id.spinner1);
 		spinner[1]=(Spinner)findViewById(R.id.spinner2);
@@ -102,6 +135,7 @@ public class MainActivity extends Activity
 		
 		radioButton1=(RadioButton) findViewById(R.id.radiobutton1);
 		radioButton2=(RadioButton) findViewById(R.id.radiobutton2);
+		textView=(TextView)findViewById(R.id.textView1);
 		//radioButton1.setSelected(true);
 		view[2].setVisibility(View.GONE);
 		spinner[2].setVisibility(View.GONE);
